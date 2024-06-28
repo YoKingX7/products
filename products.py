490dc243ad2e1586f4f415a72b0e5d0b476800ac
TAI-YU,WU <yoking0626@gmail.com>
2024-06-28 12:04:29 +0800

    add header

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:
 	if name == "q": # quit
 		break
 	price = input("請輸入商品價格 => ")
+	price = int(price) # integer
 
 	'''
 	p = []
@@ -23,7 +24,8 @@ for p in products:
 	print(p[0], "的價格是", p[1])
 
 
-with open("products.csv", "w") as f:
+with open("products.csv", "w", encoding = "utf-8") as f:
+	f.write("商品,價格\n") # 欄位名稱
 	for p in products:
-		f.write(p[0] + "," + p[1] + "\n")
+		f.write(p[0] + "," + str(p[1]) + "\n") # string
 
